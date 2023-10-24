4bb23513888af62ef22116bf2328a9f1d3dbd280
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-10-24 08:24:58 +0500

    added user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const User = require('../models/userModel')
+const Review = require('../models/reviewModel')
 //const auth = require('../middleware/auth')
 
 
@@ -82,7 +83,7 @@ const UserController = {
             }
 
             if (await bcrypt.compare(password, existingUser.password)) {
-                const token_user = { email: existingUser.email, id: existingUser._id };
+                const token_user = { email: existingUser.email, id: existingUser._id, username: existingUser.username };
                 const accessToken = generateAccessToken(token_user);
                 const response = { message: "Login successful", accessToken: accessToken}
                 res.status(201).send(response)
@@ -95,6 +96,44 @@ const UserController = {
             return res.status(500).send("Something went wrong");
         }
     },
+
+    profile: async (req, res) => {
+        try {
+
+            const username = req.params.username;
+            const currentUser = req.user.username;
+
+            if (username == currentUser) {
+                const userInfo = await User.findOne({username}).select('-password -__v -_id');
+
+                const userId = req.user.id;
+                const reviews = await Review.find({user: userId}).populate({path:'teacher', select:'name -_id'}).select('-_id -__v -user')
+
+                const userProfile = {
+                    userInfo,
+                    reviews,
+                };
+
+                return res.json(userProfile);
+            }
+            
+            /* Added functionality for looking at other people's profiles
+            else if (username != currentUser) {
+                const userId = await User.findOne({username}).select('username _id');
+                const reviews = await Review.find({user: userId}).populate({path:'teacher', select:'name -_id'}).select('-_id -__v -user')
+
+                const userProfile = {
+                    reviews,
+                };
+
+                return res.json(userProfile);
+            } */
+        
+        } catch (error) {
+          console.error(error);
+          return res.status(500).json({ message: 'Server error' });
+        }
+      },
 };
 
 
