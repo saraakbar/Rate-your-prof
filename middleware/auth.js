10bc1ae044783e346862c792b6277ab09e1ef4df
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-10-24 05:56:29 +0500

    fixed errors

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,20 @@
 const jwt = require("jsonwebtoken")
 const auth = (req,res,next) => {
-    const authHeader = req.headers['authorization'] 
-    if (authHeader == null) return res.status(401).json({ message: 'Authorization header missing' })
-    jwt.verify(authHeader, Constant.ACCESS_TOKEN_SECRET,(err, user) =>
-    {
-        if (err) return res.status(403).json({ message: err.message });
-        req.user = user;
-        next()
-  })
+    try {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        if (token == null) return res.sendStatus(401)
+      
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+          if (err) return res.status(403).send("Invalid Token")
+          req.user = user
+          next()
+        })
+
+        } catch (error) {
+        console.log(error)
+        return res.status(500).send("Something went wrong") 
+    }
 }
 
 module.exports = auth
