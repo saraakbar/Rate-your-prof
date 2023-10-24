c004aac1289228ce2dac9d183cf3598a86c256a6
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-10-24 05:54:20 +0500

    create review functionality

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,47 @@
 const Review = require('../models/reviewModel')
+const Teacher = require('../models/teacherModel')
+const jwt = require('jsonwebtoken');
 
 const reviewController = {
+    create: async (req, res) => {
+        try {
+            const {course, criteria, comment, isGrad} = req.body
+            const {teacher} = req.query
+            const authHeader = req.headers['authorization'];
+            const token = authHeader && authHeader.split(' ')[1];
+            const decodedToken = jwt.decode(token, {
+                complete: true
+            });
+            owner = decodedToken.payload.id
+            const teach = await Teacher.findOne({ID:teacher})
+
+            if (!teach) {
+                return res.status(400).send("Teacher does not exist");
+            }
+
+            
+            if (!(course && criteria && isGrad)) {
+                return res.status(400).send("All input is required");
+            }
+
+            const result = await Review.create({
+                user: owner,
+                teacher: teach._id,
+                course: course,
+                criteria: criteria,
+                comment: comment,
+                isGradReview: isGrad,
+                likes: [],
+                dislikes: []
+            })
+
+            res.status(200).json({ message: 'Review added successfully' });
+  
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Something went wrong' });
+        }
+    }
 }
 
 module.exports = reviewController;
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController'); 
+const auth = require('../middleware/auth');
+
+router.post('/create_review',auth, reviewController.create);
 
 module.exports = router;
