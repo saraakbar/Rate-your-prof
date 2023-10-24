2eac465ef51da0ed8613b8ac29a76d55f4033aa6
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-10-24 12:44:30 +0500

    added swaggerUI

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
 
+const swaggerJSdoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
+
+const options = {
+    definition:{
+        openapi: "3.0.0",
+        info:{
+            title: "Rate My Professor API",
+            version: "1.0.0",
+            description: "Rate My Professor API Information",
+            servers: [
+                {
+                    api: "http://localhost:3000/"
+        }]
+        }
+    },
+    apis: ["./routes/*.js"]
+}
+
+const swaggerSpec = swaggerJSdoc(options)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
