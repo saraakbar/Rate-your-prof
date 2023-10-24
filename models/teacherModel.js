f6a5df6ae2780a303a4a787d11ab3ba2decb696d
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-10-24 05:55:12 +0500

    added teacher ID

--- models/teacherModel.js
+++ models/teacherModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const teacherSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    ID: { type: String, required: true },
     faculty_type: { type: String, required: true },
     department: { type: String, required: true },
     position: { type: String, required: true },
