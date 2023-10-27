33a99a572f899e3b5d0d2fc5fa2294472195fe7d
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-10-28 02:56:50 +0500

    added API documentation

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -3,9 +3,147 @@ const router = express.Router();
 const teacherController = require('../controllers/teacherController'); 
 const auth = require('../middleware/auth');
 
-//router.post('/teacher/add', teacherController.create);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Teacher:
+ *       type: object
+ *       required:
+ *         - name
+ *         - ID
+ *         - faculty_type
+ *         - department
+ *         - position
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Full name of the teacher
+ *         ID:
+ *           type: string
+ *           description: Teacher's identification (ID)
+ *         faculty_type:
+ *           type: string
+ *           description: Type of faculty the teacher belongs to
+ *         department:
+ *           type: string
+ *           description: Department where the teacher works
+ *         position:
+ *           type: string
+ *           description: Position or title of the teacher
+ *       example:
+ *         name: Syed Sami Ul Ahbab
+ *         ID: syeahbab
+ *         faculty_type: Visiting Faculty
+ *         department: Computer Science
+ *         position: Visiting Faculty
+ */
+
+/**
+ * @swagger
+ * /search:
+ *   get:
+ *     security:
+ *      - BearerAuth: []
+ *     summary: Search for teachers
+ *     description: Search for teachers based on various criteria.
+ *     tags:
+ *       - Teachers
+ *     parameters:
+ *       - name: facultyType
+ *         in: query
+ *         description: Faculty type filter.
+ *         required: false
+ *         type: string
+ *       - name: department
+ *         in: query
+ *         description: Department filter.
+ *         required: false
+ *         type: string
+ *       - name: facultyName
+ *         in: query
+ *         description: Faculty name filter.
+ *         required: false
+ *         type: string
+ *       - name: alphabet
+ *         in: query
+ *         description: Alphabet filter.
+ *         required: false
+ *         type: string
+ *       - name: page
+ *         in: query
+ *         description: Page number.
+ *         required: false
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: List of teachers and total count.
+ *       404:
+ *         description: No teachers found.
+ *       500:
+ *         description: Something went wrong.
+ */
 
 router.get('/search',auth, teacherController.searchTeachers);
+
+
+/**
+ * @swagger
+ * /teacher/{ID}:
+ *   get:
+ *     security:
+ *      - BearerAuth: []
+ *     summary: Get teacher profile by ID
+ *     description: Get the profile of a teacher by their ID, including their average rating and reviews.
+ *     tags:
+ *       - Teachers
+ *     parameters:
+ *       - name: ID
+ *         in: path
+ *         description: ID of the teacher to retrieve.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Teacher profile retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 AverageRating:
+ *                   type: number
+ *                   description: The average rating of the teacher.
+ *                 teacher:
+ *                   type: object
+ *                   properties:
+ *                     position:
+ *                       type: string
+ *                       description: The teacher's position.
+ *                     name:
+ *                       type: string
+ *                       description: The teacher's name.
+ *                     department:
+ *                       type: string
+ *                       description: The teacher's department.
+ *                     faculty_type:
+ *                       type: string
+ *                       description: The faculty type of the teacher.
+ *                 reviews:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       username:
+ *                         type: string
+ *                         description: The username of the user who submitted the review.
+ *       404:
+ *         description: Teacher not found.
+ *       500:
+ *         description: Server error.
+ */
+
 
 router.get('/teacher/:ID/',auth, teacherController.profile);
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,17 +3,202 @@ const router = express.Router();
 const UserController = require('../controllers/userController'); 
 const auth = require('../middleware/auth');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - username
+ *         - erp
+ *         - email
+ *         - password
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           description: First name of the user
+ *         lastName:
+ *           type: string
+ *           description: Last name of the user
+ *         username:
+ *           type: string
+ *           description: Username of the user
+ *         erp:
+ *           type: number
+ *           description: ERP of the user
+ *         email:
+ *           type: string
+ *           description: Email of the user
+ *         password:
+ *           type: string
+ *           description: Password of the user
+ *       example:
+ *         firstName: Sara
+ *         lastName: Ebrahim
+ *         username: srakebr
+ *         erp: 22967
+ *         email: saraebrahim666@gmail.com
+ *         password: akbarS*ra7
+ */
+
 // Route for user registration
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user.
+ *     description: Register a new user with the provided information.
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               erp:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Registration Successful. Please login.
+ *       400:
+ *         description: Bad Request. Check the request body for missing or invalid fields.
+ *       409:
+ *         description: Conflict. User already exists with the provided email or username.
+ *       500:
+ *         description: Internal Server Error. Something went wrong.
+ */
 router.post('/register', UserController.register);
 
 // Route for user login
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Log in a user.
+ *     description: Authenticate a user using their email and password.
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Login successful. Returns an access token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A success message.
+ *                 accessToken:
+ *                   type: string
+ *                   description: An access token for authenticated requests.
+ *       400:
+ *         description: Bad Request. Invalid credentials or missing fields.
+ *       404:
+ *         description: Not Found. User not found.
+ *       500:
+ *         description: Internal Server Error. Something went wrong.
+ */
 router.post('/login', UserController.login);
 
 // Route for user profile
+/**
+ * @swagger
+ * /user/{username}/profile:
+ *   get:
+ *     summary: Get current user's profile.
+ *     description: Retrieve the profile information and reviews for a logged in user.
+ *     tags:
+ *       - User
+ *     security:
+ *      - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The username of the current.
+ *     responses:
+ *       200:
+ *         description: User profile information and reviews.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userInfo:
+ *                   type: object
+ *                   description: User information excluding password.
+ *                 reviews:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       teacher:
+ *                         type: object
+ *                         properties:
+ *                           name:
+ *                             type: string
+ *       403:
+ *         description: Forbidden. User can only access their own profile.
+ *       500:
+ *         description: Internal Server Error. Something went wrong.
+ */
 router.get('/user/:username/profile',auth, UserController.profile);
 
-//Route for delete profile
-//router.delete('user/:username/delete',auth, UserController.delete);
+/**
+ * @swagger
+ * /user/{username}/delete:
+ *   delete:
+ *     summary: Delete current user's profile.
+ *     description: Delete the user profile associated with the provided username.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The username of the user profile to be deleted.
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User deleted successfully.
+ *       403:
+ *         description: Forbidden. You are not authorized to delete this profile.
+ *       500:
+ *         description: Internal Server Error. Something went wrong.
+ */
+router.delete('user/:username/delete',auth, UserController.delete);
 
 //Route for update profile
 //router.patch('user/:username/update',auth, UserController.update);
