a0877ac716ac9620543b36accfdf32ebf4797924
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-01 08:16:35 +0500

    admin verification added

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,4 +17,12 @@ const auth = (req,res,next) => {
     }
 }
 
-module.exports = auth
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({ message: 'Access forbidden. Admin role required.' });
+  }
+};
+
+module.exports = {auth, isAdmin}
