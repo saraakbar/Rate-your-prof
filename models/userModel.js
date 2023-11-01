10b425c3b686f2ba941692124517f097bd7dcef2
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-01 08:18:15 +0500

    admin role added

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
     erp: {type: Number, unique: true, required: true},
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
+    role:{ type: String, enum: ['user', 'admin'],required: true}
 })
 
 const User = mongoose.model('User', userSchema);
