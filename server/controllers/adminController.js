74fc00ee3ca730d8db5d4e4c690feb7dd70c7394
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-04 06:53:31 +0500

    admin login and view reports added

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,8 +1,54 @@
 const User = require('../models/userModel')
 const Review = require('../models/reviewModel')
 const Report = require('../models/reportedModel')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2200s' })
+}
 
 const adminController = {
+    login: async (req, res) => {
+        const { username, password } = req.body;
+        try{
+            const admin = await User.findOne({username: username});
+            if (!admin) return res.status(400).send("User does not exist");
+
+            if (await bcrypt.compare(password, admin.password)) {
+                const token_user = {id: admin._id, username: admin.username, role: admin.role};
+                const accessToken = generateAccessToken(token_user);
+                const response = { message: "Login successful"}
+                console.log(accessToken);
+                res.status(201).send(response)
+            } else {
+                return res.status(400).send('Invalid credentials');
+            }
+        } catch (error){
+            console.log(error);
+            return res.status(500).send("Server Error");
+        }
+    },
+
+    reports: async (req, res) => {
+        try{
+            const reports = await Report.find()
+            .populate({path: 'user',select:'username'})
+            .populate({
+                path: 'review',
+                select: '_id',
+                populate: {
+                    path: 'user',
+                    select: 'username'
+                }
+            })
+            .select('-__v -_id')
+            res.status(200).send(reports);
+        } catch (error){
+            console.log(error);
+            return res.status(500).send("Server Error");
+        }
+    },
 }
 
 module.exports = adminController
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const AdminController = require('../controllers/adminController'); 
 const {auth,isAdmin} = require('../middleware/auth');
 
-//router.get('/admin/reports',auth,isAdmin, AdminController.getReports);
+router.post('/admin/login',AdminController.login);
+router.get('/admin/reports',auth,isAdmin, AdminController.reports);
 //router.get('/admin/search',auth,isAdmin, AdminController.searchUsers);
 //router.get('/admin/user/:username',auth,isAdmin, AdminController.getUser);
 //router.get('/admin/review/:id',auth,isAdmin, AdminController.getReview);
 //router.patch('/admin/report/:id',auth,isAdmin, AdminController.resolveReport);
-//router.delete('/admin/user/:username',auth,isAdmin, AdminController.deleteUser);
+//router.patch('/admin/user/:username',auth,isAdmin, AdminController.suspendUser);
 //router.delete('/admin/review/:id',auth,isAdmin, AdminController.deleteReview);
 //router.delete('/admin/cleanup',auth,isAdmin, AdminController.deleteReports);
 
