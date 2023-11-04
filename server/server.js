5616e20f950b088fd2ff9a6c3bbc2881b42e633a
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-04 05:50:48 +0500

    commented swagger

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
-const cors = require('cors');
+//const cors = require('cors');
 
-const swaggerJSdoc = require('swagger-jsdoc')
+/* const swaggerJSdoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
 
 const options = {
@@ -29,24 +29,20 @@ const options = {
 
 
 const swaggerSpec = swaggerJSdoc(options)
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec)) */
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json())
 
 const userRoute = require('./routes/userRoute');
 const teacherRoute = require('./routes/teacherRoute');
 const reviewRoute = require('./routes/reviewRoute');
 const adminRoute = require('./routes/adminRoute');
-
-app.get('/welcome', (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
 
 app.use(userRoute);
 app.use(teacherRoute);
