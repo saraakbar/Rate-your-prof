b1f7311e96bc0342db06a82c8f6098d6adfdbb96
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-09 04:37:54 +0500

    front end support added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
-//const cors = require('cors');
+const cors = require('cors');
 
 /* const swaggerJSdoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
@@ -36,13 +36,18 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json())
 
 const userRoute = require('./routes/userRoute');
 const teacherRoute = require('./routes/teacherRoute');
 const reviewRoute = require('./routes/reviewRoute');
 const adminRoute = require('./routes/adminRoute');
+
+app.get("/", (req, res) => {
+  const response = "Welcome to Rate Your Professor";
+    res.status(200).json({ message: response });
+  });
 
 app.use(userRoute);
 app.use(teacherRoute);
