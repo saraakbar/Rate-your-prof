611f566b28caa5e1bab34db95e1739f390c33454
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-10 01:46:59 +0500

    toastify added

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Login from "./views/Login.js";
 import Register from "./views/Register.js";
@@ -9,6 +11,7 @@ import LandingPage from "./views/LandingPage.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+  <ToastContainer/>
     <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
