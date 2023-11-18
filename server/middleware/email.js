5a62cbf544778b8b39215b6d78586f7b2024697c
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2023-11-18 05:38:16 +0500

    packages added + send emails for reset password

---
+++ server/middleware/email.js
@@ -0,0 +1,57 @@
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+Token = require('../models/tokenModel')
+
+function sendEmail(mailOptions) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.FROM_EMAIL,
+          pass: process.env.FROM_EMAIL_PASSWORD,
+        }
+      });
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (error, result) => {
+            if (error) return reject(error);
+            return resolve(result);
+        });
+    });
+}
+
+function generateVerificationToken(user, verify) {
+    let payload = {
+        userId: `${user._id}`,
+        token: crypto.randomBytes(20).toString('hex'),
+        verify: verify,
+    };
+
+    return new Token(payload);
+};
+
+async function sendVerificationEmail(user, req, res, verify){
+    try{
+        const token = generateVerificationToken(user, verify);
+
+        // Save the verification token
+        await token.save();
+        
+        let subject = "Reset Password";
+        let to = user.Email;
+        let from = process.env.FROM_EMAIL;
+        let html= "";
+        let link= "";
+    
+        //link="http://"+req.headers.host+"/auth/forgotpass/verify/"+token.token;
+        html = `<p>Hi ${user.username}<p><br><p>Please click on the following <a href="${link}">link</a> to reset your password.</p> 
+        <br><p>If you did not request this, please ignore this email.</p>`;
+        
+        await sendEmail({to, from, subject, html});
+        res.status(200).json({message: 'An email has been sent to ' + user.Email + '.'});
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Server Error"})
+    }
+}
+module.exports = {
+    sendEmail,  generateVerificationToken, sendVerificationEmail
+}
